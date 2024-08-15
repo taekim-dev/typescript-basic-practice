3ebfb9ba9fb1d64b621216282e749990f53fc265
Taewan Kim <taekim.dev@gmail.com>
2024-08-16 07:10:33 +0800

    add memoize implementation

--- src/problems/4.memoize-function.ts
+++ src/problems/4.memoize-function.ts
@@ -1,3 +1,15 @@
-export const memoize = (input: any) : any => {
-    return input
+export const memoize = <T extends (...args: any[]) => any>(fn: T): (...args: Parameters<T>) => ReturnType<T> => {
+    const cache = new Map<string, ReturnType<T>>();
+
+    return function memoized(this: unknown, ...args: Parameters<T>): ReturnType<T> {
+        const key = JSON.stringify(args);
+
+        if (cache.has(key)){
+            return cache.get(key)!;
+        }
+
+        const res = fn.apply(this, args);
+        cache.set(key, res);
+        return res;
+    }
 }
