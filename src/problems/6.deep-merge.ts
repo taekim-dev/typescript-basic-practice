0daeb42a8f3d450abe2118e55687947ba197d13f
Taewan Kim <taekim.dev@gmail.com>
2024-08-20 08:01:32 +0800

    apply isplainobject util to deep merge

--- src/problems/6.deep-merge.ts
+++ src/problems/6.deep-merge.ts
@@ -1,9 +1,11 @@
+import { isPlainObject } from "./0.plain-object";
+
 export function deepMerge(obj1: { [key: string]: any }, obj2: { [key: string]: any }): { [key: string]: any } {
     const newObj: { [key: string]: any } = { ...obj1 };
 
     for (const [key, value] of Object.entries(obj2)) {
         if (newObj.hasOwnProperty(key)) {
-            if (typeof newObj[key] === 'object' && typeof value === 'object') {
+            if (isPlainObject(newObj[key]) && isPlainObject(value)) {
                 newObj[key] = deepMerge(newObj[key], value);
             } else {
                 newObj[key] = value; // Overwrite primitive or non-object values
