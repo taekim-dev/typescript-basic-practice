efc94757d7703b9531e69b7cc2c1038b189d6aca
Taewan Kim <taekim.dev@gmail.com>
2024-08-23 08:06:18 +0800

    add ininite scroll skeleton class

---
+++ src/problems/7.infinite-scroll.ts
@@ -0,0 +1,35 @@
+class InfiniteScroll {
+    private threshold : number;
+    private fetchData : () => Promise<void>;
+    private isLoading : boolean;
+
+    constructor () {
+
+    }
+
+    initScrollListener() {
+
+    }
+
+    private async onScroll() {
+
+    }
+
+    public destroy() {
+
+    }
+
+    async fetchData () {
+
+    }
+}
+
+
+let page = 1;
+
+async function fetchData() {
+
+}
+
+// Initialize Infinite Scroll
+const infiniteScroll = new InfiniteScroll(300, fetchData);
