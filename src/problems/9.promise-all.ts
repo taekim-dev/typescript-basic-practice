b31ad329ed46563345e1c1128c7b57805e1de8df
Taewan Kim <taekim.dev@gmail.com>
2024-08-27 08:52:05 +0800

    add typescript annotations

--- src/problems/9.promise-all.ts
+++ src/problems/9.promise-all.ts
@@ -1,21 +1,23 @@
 export function PromiseAll<T>(promises: Promise<T>[]) : Promise<T[]> {
     return new Promise((resolve, reject) => {
         let res : T[] = [];
-        let completed = 0;
+        let completed : number = 0;
 
         if (promises.length === 0) {
             resolve([]);
         }
 
-        promises.forEach((promise, index) => {
-            promise.then(value => {
+        promises.forEach((promise : Promise<T>, index : number) => {
+            promise.then((value : T) => {
                 res[index] = value;
                 completed++;
                 if (completed === promises.length) {
                     resolve(res);
                 }
 
-            }).catch(error => reject(error))
+            }).catch((error : any) => {
+                reject(error);
+            })
         })
         
     });
