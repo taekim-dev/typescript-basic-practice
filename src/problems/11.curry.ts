29c9a2bfbb6749c79048f0922552057084584e24
Taewan Kim <taekim.dev@gmail.com>
2024-08-29 08:11:11 +0800

    add curry and test cases

---
+++ src/problems/11.curry.ts
@@ -0,0 +1,8 @@
+// Problem:
+// Implement a curry function that transforms a function of multiple arguments into a sequence of functions, 
+// each taking a single argument. 
+// This is often used in functional programming to create more reusable and flexible functions.
+
+export function curry(fn: Function): Function {
+
+}
---
+++ src/tests/11.curry.test.ts
@@ -0,0 +1,10 @@
+import { curry } from '../problems/11.curry';
+
+test('should transform multiple arguments function', async () => {
+    const inputFunction = (x : number, y : number, z : number ) => x + y + z;
+    const curried = curry(inputFunction);
+
+    expect(curried(1, 2, 3)).toEqual(6);
+    expect(curried(1)(2)(3)).toEqual(6);
+    expect(curried(1, 2) (3)).toEqual(6);
+});
