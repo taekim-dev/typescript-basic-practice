755aa28a02833cd956a6956ce339e60283553385
Taewan Kim <taekim.dev@gmail.com>
2024-08-31 08:20:16 +0800

    add pipe test case

---
+++ src/problems/12.pipe.ts
@@ -0,0 +1,8 @@
+// Problem:
+// Implement a pipe function that performs left-to-right function composition. 
+// It takes a sequence of functions as input and returns a new function that applies 
+// each function to the result of the previous function, starting with an initial value.
+
+export function pipe(...fns: Function[]): Function {
+    
+}
---
+++ src/tests/12.pipe.test.ts
@@ -0,0 +1,15 @@
+// Problem:
+// Implement a pipe function that performs left-to-right function composition. 
+// It takes a sequence of functions as input and returns a new function that applies 
+// each function to the result of the previous function, starting with an initial value.
+
+import { pipe } from '../problems/12.pipe';
+
+test('should chain multiple functions', () => {
+    const sum = (a: number): number => a + 2;
+    const subtract = (a: number): number => a - 1;
+    const multiply = (a: number): number => a * 3;
+
+    const result = pipe(sum, subtract, multiply)(5);
+    expect(result).toBe(18);
+});
