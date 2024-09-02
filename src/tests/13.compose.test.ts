bcef70a6198320bae312f0c01dfd07e4a0d3ad96
Taewan Kim <taekim.dev@gmail.com>
2024-09-02 08:24:07 +0800

    add more test cases

--- src/tests/13.compose.test.ts
+++ src/tests/13.compose.test.ts
@@ -8,3 +8,16 @@ test('should chain multiple functions', () => {
 
     expect(composed(5)).toEqual(16);
 });
+
+test('should handle a single function', () => {
+    const square = (a : number) : number => a * a;
+    const composed = compose(square);
+
+    expect(composed(5)).toBe(25);
+});
+
+test('should handle no function', () => {
+    const composed = compose();
+
+    expect(composed(5)).toBe(5);
+});
