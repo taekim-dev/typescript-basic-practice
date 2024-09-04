1b500849da3d8cc199e6a83da03347a758a1ed34
Taewan Kim <taekim.dev@gmail.com>
2024-09-04 08:29:29 +0800

    add another test

--- src/tests/15.memoize.test.ts
+++ src/tests/15.memoize.test.ts
@@ -8,3 +8,16 @@ test('should memoize the function result', () => {
     expect(memoized(5)).toEqual(7); //from cache
     expect(sum).toHaveBeenCalledTimes(1);
 });
+
+test('should memoize the function results', () => {
+    const sum = jest.fn((a: number): number => a + 2);
+    const memoized = memoize(sum); 
+
+    expect(memoized(5)).toEqual(7); 
+    expect(memoized(5)).toEqual(7); //from cache
+    expect(memoized(1)).toEqual(3);
+    expect(memoized(5)).toEqual(7);
+    expect(memoized(1)).toEqual(3);
+    expect(memoized(1)).toEqual(3);
+    expect(sum).toHaveBeenCalledTimes(2);
+});
