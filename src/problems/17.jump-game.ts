3b6ca5022211c7d3222ffc534c0629ce8159585a
Taewan Kim <taekim.dev@gmail.com>
2024-09-07 07:34:30 +0800

    apply greedy algorithm

--- src/problems/17.jump-game.ts
+++ src/problems/17.jump-game.ts
@@ -1,17 +1,9 @@
 export function canJump(nums: number[]): boolean {
-    
-    const positions : Boolean[] = Array(nums.length).fill(false);
-
-    for(let i = 0; i < positions.length-1; i++) {
-        const capacity = nums[i];
-        if ((i + capacity) >= (positions.length-1)) return true;
-
-        for (let j = 1; j <= capacity; j++) {
-            if (positions[i + j] === false) {
-                positions[i + j] = true;
-            }
-        }
+    let farthest = 0;
+    for (let i = 0; i < nums.length; i++) {
+        if (i > farthest) return false;
+        farthest = Math.max(i + nums[i], farthest);
+        if (farthest >= nums.length - 1) return true;
     }
-
     return false;
 };
