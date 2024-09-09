18c24b80a02cf2e0a5c8fb066bc773d3ac20a12a
Taewan Kim <taekim.dev@gmail.com>
2024-09-09 08:20:09 +0800

    add implementation

--- src/tests/19.happy-number.test.ts
+++ src/tests/19.happy-number.test.ts
@@ -4,3 +4,9 @@ test('should handle basic cases', () => {
     expect(isHappy(2)).toEqual(false);
     expect(isHappy(19)).toEqual(true);
 });
+
+test('should handle complex cases', () => {
+    expect(isHappy(200001)).toEqual(false);
+    expect(isHappy(999999)).toEqual(false);
+    expect(isHappy(986543210)).toEqual(true);
+});
