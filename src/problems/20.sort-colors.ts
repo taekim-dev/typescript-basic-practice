cb9f436d0f86117f71f567062b9a11d9092e837a
Taewan Kim <taekim.dev@gmail.com>
2024-09-22 09:04:38 +0800

    add optimal solution

--- src/problems/20.sort-colors.ts
+++ src/problems/20.sort-colors.ts
@@ -2,26 +2,22 @@
  Do not return anything, modify nums in-place instead.
  */
 export function sortColors(nums: number[]): number[] {
-    let counterZero : number = 0;
-    let counterOne : number = 0;
-    let counterTwo : number = 0;
+    let low = 0; 
+    let mid = 0;
+    let high = nums.length - 1;
 
-    nums.forEach(number => {
-        if (number === 0) counterZero++;
-        if (number === 1) counterOne++;
-        if (number === 2) counterTwo++;
-    })
-
-    addNumbers(0, counterZero, 0, nums);
-    addNumbers(counterZero, counterZero + counterOne, 1, nums);
-    addNumbers(counterZero + counterOne, counterZero + counterOne + counterTwo, 2, nums);
-    
-    function addNumbers(start : number, end : number, number: number, nums : number[]) : void {
-        for(let i = start; i < end; i++) {
-            nums[i] = number;
+    while (mid <= high) {
+        if (nums[mid] === 0) {
+            [nums[low], nums[mid]] = [nums[mid], nums[low]];
+            low++;
+            mid++;
+        } else if (nums[mid] === 1) {
+            mid++;
+        } else if (nums[mid] === 2) {
+            [nums[mid], nums[high]] = [nums[high], nums[mid]];
+            high--;
         }
     }
 
-    console.log(nums)
     return nums;
 };
